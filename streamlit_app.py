55bd91b370c620eadd6f0b2e9885c803c43bf523
zak1234lomas-debug <zak1234lomas@gmail.com>
2026-03-23 18:39:48 +0000

    Fix layout for win probability display in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -312,5 +312,7 @@ col2.write(f'Win Probability with GA 1: {GA1PWin.round(2)}%')
 
 col1, col2 = st.columns(2)
 col1.write(f'Win Probability with a made 1: {TeamWin1P.round(2)}%')
-col1.write(f'Win Probability with a made 2: {TeamWin2P.round(2)}%')
+col2.write(f'Win Probability with a made 2: {TeamWin2P.round(2)}%')
 st.write(f'Win Probability with a miss: {TeamWinMiss.round(2)}%')
+
+<div style="text-align: center"> st.write(f'Win Probability with a miss: {TeamWinMiss.round(2)}%') </div>
